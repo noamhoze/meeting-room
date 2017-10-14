555fd3c7b4a99281c3f2fa0d72b2e6bd17c54b68
Gilad Hoze <gilad2210@gmail.com>
2017-10-15 00:29:16 +0300

    Added some docs on the config file

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ General configuration for the project.
 # DB related config
 NUMBER_OF_PEOPLE_FIELD = 'number_of_people'
 ROOM_FIELD = "name"
-DB_PATH = r"C:\Users\Gilad\PycharmProjects\free-rooms-server\free_rooms_db.json"
 ROOMS_DATA_TABLE = "rooms_data"
+# TODO: To be configured to the db path.
+DB_PATH = r"C:\Users\Gilad\PycharmProjects\free-rooms-server\free_rooms_db.json"
 
 # Server related config
+# TODO: To be configured to the real server's ip and port.
 HOST = "127.0.0.1"
 PORT = 9999
 
