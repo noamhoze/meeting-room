151baf9afacab26ddd6a49f763d5df9d9223035f
Gilad Hoze <gilad2210@gmail.com>
2017-10-14 22:51:09 +0300

    First version of alexa meeting_room app

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+General configuration for the project.
+"""
+
+# DB related config
+NUMBER_OF_PEOPLE_FIELD = 'number_of_people'
+ROOM_FIELD = "name"
+DB_PATH = r"C:\Users\Gilad\PycharmProjects\free-rooms-server\free_rooms_db.json"
+ROOMS_DATA_TABLE = "rooms_data"
+
+# Server related config
+HOST = "127.0.0.1"
+PORT = 9999
+
+# General config
+UTF_FORMAT = 'UTF-8'
